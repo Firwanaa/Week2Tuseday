283fcda5d1f14278b8c3119504cb65fe58524afd
Firwanaa <firwanaa@sheridancollege.ca>
2020-06-02 19:12:58 -0400

    This is my Second commit - Test 2

--- src/week2tuesday/Week2Tuesday.java
+++ src/week2tuesday/Week2Tuesday.java
@@ -17,6 +17,7 @@ public class Week2Tuesday {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("learn github");
+        System.out.println("Second Message");
     }
     
 }
