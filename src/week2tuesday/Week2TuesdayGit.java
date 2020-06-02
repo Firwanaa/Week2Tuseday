37b3b0cef50eece34e87879239d264d4182454c9
Firwanaa <firwanaa@sheridancollege.ca>
2020-06-02 19:06:07 -0400

    This is my First commit - Test 1

---
+++ src/week2tuesday/Week2TuesdayGit.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week2tuesday;
+
+/**
+ *
+ * @author QQ
+ */
+public class Week2TuesdayGit {
+    private String week2Git;
+
+    /**
+     * @return the week2Git
+     */
+    public String getWeek2Git() {
+        return week2Git;
+    }
+
+    /**
+     * @param week2Git the week2Git to set
+     */
+    public void setWeek2Git(String week2Git) {
+        this.week2Git = week2Git;
+    }
+    
+    
+}
